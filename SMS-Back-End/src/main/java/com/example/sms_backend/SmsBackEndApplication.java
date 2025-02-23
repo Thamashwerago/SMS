7b6b63e090dff0639eb2d16733dd6a5ba771e76c
Tharindu Danushka <datdsgunarathna@gmail.com>
2025-02-23 20:20:02 +0530

    new

---
+++ SMS-Back-End/src/main/java/com/example/sms_backend/SmsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.sms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsBackEndApplication.class, args);
+    }
+
+}
