e44c5c069f662a766f0281973ad61c5afdd02f0d
Tharindu Danushka <datdsgunarathna@gmail.com>
2025-04-05 21:32:07 +0530

    new

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import AdminRoutes from './routes/AdminRoutes';
+import AdminRoutes from './routes/AdminRoutes';
 // import AuthRoutes from './routes/AuthRoutes';
 import StudentRoutes from './routes/StudentRoutes';
 import TeacherRoutes from './routes/TeacherRoutes';
@@ -9,7 +9,7 @@ const App: React.FC = () => {
   return (
     <Router>
       {/* <AuthRoutes /> */}
-      {/* <AdminRoutes /> */}
+      <AdminRoutes />
       <StudentRoutes />
       <TeacherRoutes />
 
