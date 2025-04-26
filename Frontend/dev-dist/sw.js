b7471f709759e5a47c649abad70ce24406a53d85
Thamashwerago <62113345+Thamashwerago@users.noreply.github.com>
2025-04-26 11:18:19 +0530

    update

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.5bf1fii4jgo"
+    "revision": "0.ai61momo5pg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
